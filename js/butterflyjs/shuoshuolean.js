e692940a77561838e0adfcd18701c90712f40ee5
mt-balck <3300652636@qq.com>
2022-09-10 17:43:57 +0800

    Site updated: 2022-09-10 17:43:52

---
+++ js/butterflyjs/shuoshuolean.js
@@ -0,0 +1,5 @@
+new Artitalk({
+    appId: '5v20XpE5NyoD7YZShJrn7hPb-9Nh9j0Va', // Your LeanCloud appId
+    appKey: 'cJZ6vaiKLth4WmcbsRNlT3dV', // Your LeanCloud appKey
+    serverURL: 'https://leancloud.lcoo.cc'
+})
